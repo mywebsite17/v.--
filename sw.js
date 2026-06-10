f85109b1492689574794566eb4f06598125e0190
mywebsite17 <omarsalam2214+omar@gmail.com>
2026-06-10 19:50:49 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,37 @@
-const CACHE_NAME = 'iotv app-v2';
-const urlsToCache = [ './', './index.html', './icon-192.png', './icon-512.png', './manifest.json' ];
+const CACHE_NAME = 'pwa-cache-v883';
+const urlsToCache = [ './', './index.html', './offline.html', './icon-192.png', './icon-512.png', './manifest.json' ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request).catch(() => {
+          if (event.request.mode === 'navigate') {
+            return caches.match('./offline.html');
+          }
+        });
+      })
+  );
 });
